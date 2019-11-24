ef9aa13179d5074af015d14497aff6dbcb1a3d11
Martin Redolatti <martinredolatti@gmail.com>
2019-11-24 18:32:17 -0300

    fix race condition in asynctask

--- asynctask/asynctasks.go
+++ asynctask/asynctasks.go
@@ -3,6 +3,7 @@ package asynctask
 import (
 	"fmt"
 	"github.com/splitio/go-toolkit/logging"
+	"sync/atomic"
 	"time"
 )
 
@@ -11,7 +12,7 @@ import (
 type AsyncTask struct {
 	task     func(l logging.LoggerInterface) error
 	name     string
-	running  bool
+	running  atomic.Value
 	incoming chan int
 	period   int
 	onInit   func(l logging.LoggerInterface) error
@@ -24,22 +25,31 @@ const (
 	taskMessageWakeup
 )
 
+func (t *AsyncTask) _running() bool {
+	res, ok := t.running.Load().(bool)
+	if !ok {
+		t.logger.Error("Error parsing async task status flag")
+		return false
+	}
+	return res
+}
+
 // Start initiates the task. It wraps the execution in a closure guarded by a call to recover() in order
 // to prevent the main application from crashin if something goes wrong while the sdk interacts with the backend.
 func (t *AsyncTask) Start() {
 
-	if t.running {
+	if t._running() {
 		if t.logger != nil {
 			t.logger.Warning("Task %s is already running. Aborting new execution.", t.name)
 		}
 		return
 	}
-	t.running = true
+	t.running.Store(true)
 
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				t.running = false
+				t.running.Store(false)
 				if t.logger != nil {
 					t.logger.Error(fmt.Sprintf(
 						"AsyncTask %s is panicking! Delaying execution for %d seconds (1 period)",
@@ -65,7 +75,7 @@ func (t *AsyncTask) Start() {
 		}
 
 		// Task execution
-		for t.running {
+		for t._running() {
 			// Run the wrapped task and handle the returned error if any.
 			err := t.task(t.logger)
 			if err != nil && t.logger != nil {
@@ -77,7 +87,7 @@ func (t *AsyncTask) Start() {
 			case msg := <-t.incoming:
 				switch msg {
 				case taskMessageStop:
-					t.running = false
+					t.running.Store(false)
 				case taskMessageWakeup:
 				}
 			case <-time.After(time.Second * time.Duration(t.period)):
@@ -112,7 +122,7 @@ func (t *AsyncTask) WakeUp() error {
 
 // IsRunning returns true if the task is currently running
 func (t *AsyncTask) IsRunning() bool {
-	return t.running
+	return t._running()
 }
 
 // NewAsyncTask creates a new task and returns a pointer to it
@@ -127,12 +137,12 @@ func NewAsyncTask(
 	t := AsyncTask{
 		name:     name,
 		task:     task,
-		running:  false,
 		period:   period,
 		onInit:   onInit,
 		onStop:   onStop,
 		logger:   logger,
 		incoming: make(chan int, 10),
 	}
+	t.running.Store(false)
 	return &t
 }
--- asynctask/asynctasks_test.go
+++ asynctask/asynctasks_test.go
@@ -3,22 +3,26 @@ package asynctask
 import (
 	"errors"
 	"github.com/splitio/go-toolkit/logging"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestAsyncTaskNormalOperation(t *testing.T) {
 
-	onInit := false
-	onExecution := false
-	onStop := false
+	onInit := atomic.Value{}
+	onInit.Store(false)
+	onExecution := atomic.Value{}
+	onExecution.Store(false)
+	onStop := atomic.Value{}
+	onStop.Store(false)
 
 	task1 := NewAsyncTask(
 		"testTask1",
-		func(l logging.LoggerInterface) error { onExecution = true; return nil },
+		func(l logging.LoggerInterface) error { onExecution.Store(true); return nil },
 		1,
-		func(l logging.LoggerInterface) error { onInit = true; return nil },
-		func(l logging.LoggerInterface) { onStop = true },
+		func(l logging.LoggerInterface) error { onInit.Store(true); return nil },
+		func(l logging.LoggerInterface) { onStop.Store(true) },
 		logging.NewLogger(nil),
 	)
 
@@ -34,18 +38,17 @@ func TestAsyncTaskNormalOperation(t *testing.T) {
 		t.Error("Task should be stopped")
 	}
 
-	if !onInit {
+	if !onInit.Load().(bool) {
 		t.Error("Initialization hook not executed")
 	}
 
-	if !onExecution {
+	if !onExecution.Load().(bool) {
 		t.Error("Main task function not executed")
 	}
 
-	if !onStop {
+	if !onStop.Load().(bool) {
 		t.Error("After execution function not executed")
 	}
-
 }
 
 func TestAsyncTaskPanics(t *testing.T) {
@@ -106,10 +109,11 @@ func TestAsyncTaskPanics(t *testing.T) {
 
 func TestAsyncTaskErrors(t *testing.T) {
 	// Error in execution skips one iteration
-	res := 0
+	res := atomic.Value{}
+	res.Store(0)
 	task1 := NewAsyncTask(
 		"testTask1",
-		func(l logging.LoggerInterface) error { res++; return errors.New("") },
+		func(l logging.LoggerInterface) error { res.Store(res.Load().(int) + 1); return errors.New("") },
 		1,
 		func(l logging.LoggerInterface) error { return nil },
 		func(l logging.LoggerInterface) {},
@@ -120,14 +124,14 @@ func TestAsyncTaskErrors(t *testing.T) {
 	time.Sleep(time.Second * 3)
 	task1.Stop()
 
-	if res < 2 {
+	if res.Load().(int) < 2 {
 		t.Error("Task should have executed at least two times")
 	}
 
-	res = 0
+	res.Store(0)
 	task2 := NewAsyncTask(
 		"testTask1",
-		func(l logging.LoggerInterface) error { res++; return nil },
+		func(l logging.LoggerInterface) error { res.Store(res.Load().(int) + 1); return nil },
 		1,
 		func(l logging.LoggerInterface) error { return errors.New("") },
 		func(l logging.LoggerInterface) {},
@@ -136,16 +140,17 @@ func TestAsyncTaskErrors(t *testing.T) {
 
 	task2.Start()
 	time.Sleep(2 * time.Second)
-	if res != 0 {
+	if res.Load().(int) != 0 {
 		t.Error("Task should have never executed if there was an error when calling onInit()")
 	}
 }
 
 func TestAsyncTaskWakeUp(t *testing.T) {
-	res := 0
+	res := atomic.Value{}
+	res.Store(0)
 	task1 := NewAsyncTask(
 		"testTask1",
-		func(l logging.LoggerInterface) error { res++; return nil },
+		func(l logging.LoggerInterface) error { res.Store(res.Load().(int) + 1); return nil },
 		20,
 		func(l logging.LoggerInterface) error { return nil },
 		func(l logging.LoggerInterface) {},
@@ -163,7 +168,7 @@ func TestAsyncTaskWakeUp(t *testing.T) {
 
 	time.Sleep(time.Second * 2)
 
-	if res != 4 {
+	if res.Load().(int) != 4 {
 		t.Errorf("Task shuld have executed 4 times. It ran %d times", res)
 	}
 }
