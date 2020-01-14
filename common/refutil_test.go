28026a058a4b2530fffb7370fd6a019342a93276
Matias Melograno <matiasmelograno@split.io>
2020-01-14 15:18:20 -0300

    added int

--- common/refutil_test.go
+++ common/refutil_test.go
@@ -10,3 +10,17 @@ func TestStringRef(t *testing.T) {
 		t.Error("Wrong string reference")
 	}
 }
+
+func TestInt64Ref(t *testing.T) {
+	a := int64(3)
+	if a != *Int64Ref(a) {
+		t.Error("Wrong int64 reference")
+	}
+}
+
+func TestIntRef(t *testing.T) {
+	a := int(43)
+	if a != *IntRef(a) {
+		t.Error("Wrong int reference")
+	}
+}
