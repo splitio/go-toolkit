4736dc081c1f4788e104110bb139315c9d8c31b4
Matias Melograno <matiasmelograno@split.io>
2020-02-19 17:42:48 -0300

    added encoding utils

---
+++ hasher/util_test.go
@@ -0,0 +1,22 @@
+package hasher
+
+import "testing"
+
+func TestEncode(t *testing.T) {
+	hash, err := Encode(nil, "something")
+	if hash != "" {
+		t.Error("Unexpected result")
+	}
+	if err == nil || err.Error() != "Hasher could not be nil" {
+		t.Error("Unexpected error message")
+	}
+
+	hasher := NewMurmur332Hasher(0)
+	hash2, err := Encode(hasher, "something")
+	if err != nil {
+		t.Error("It should not return error")
+	}
+	if hash2 != "NDE0MTg0MjI2MQ==" {
+		t.Error("Unexpected result")
+	}
+}
