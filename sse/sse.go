441e053cb1a1e019c9ddb37a7c77f601279b4579
Martin Redolatti <martinredolatti@gmail.com>
2020-09-07 10:47:06 -0300

    handle EOF in sse connection

--- sse/sse.go
+++ sse/sse.go
@@ -87,7 +87,7 @@ func parseData(raw []byte) (map[string]interface{}, error) {
 
 func (l *SSEClient) readEvent(reader *bufio.Reader) (map[string]interface{}, error) {
 	line, err := reader.ReadBytes('\n')
-	if err != nil && err != io.EOF {
+	if err != nil {
 		return nil, err
 	}
 
