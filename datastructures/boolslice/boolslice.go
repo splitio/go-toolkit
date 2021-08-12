2c92dc44b7a1c9960917c8c4c3306ca4318dca97
Matias Melograno <matiasmelograno@split.io>
2021-08-12 14:35:52 -0300

    added boolslice

---
+++ datastructures/boolslice/boolslice.go
@@ -0,0 +1,73 @@
+package boolslice
+
+import (
+	"errors"
+)
+
+// ErrorOutOfBounds err
+var ErrorOutOfBounds error = errors.New("out of bounds")
+
+// BoolSlice struct
+type BoolSlice struct {
+	raw  []byte
+	size int
+}
+
+// NewBoolSlice builds new empty BoolSlice
+func NewBoolSlice(size int) (*BoolSlice, error) {
+	if size%8 != 0 {
+		return nil, errors.New("size must be a multiple of 8")
+	}
+	return &BoolSlice{
+		raw:  make([]byte, size/8),
+		size: size,
+	}, nil
+}
+
+// Rebuild generates new BoolSlice from data
+func Rebuild(size int, data []byte) (*BoolSlice, error) {
+	return &BoolSlice{
+		raw:  data,
+		size: size,
+	}, nil
+}
+
+// Set sets a bit from index passed
+func (b *BoolSlice) Set(index int) error {
+	if index > b.size {
+		return ErrorOutOfBounds
+	}
+
+	internal := index / 8
+	offset := index % 8
+	b.raw[internal] |= (1 << offset)
+	return nil
+}
+
+// Clear clears a bit from index passed
+func (b *BoolSlice) Clear(index int) error {
+	if index > b.size {
+		return ErrorOutOfBounds
+	}
+
+	internal := index / 8
+	offset := index % 8
+	b.raw[internal] &= ((1 << offset) ^ 0xFF)
+	return nil
+}
+
+// Get gets value from index passed
+func (b *BoolSlice) Get(index int) (bool, error) {
+	if index > b.size {
+		return false, ErrorOutOfBounds
+	}
+
+	internal := index / 8
+	offset := index % 8
+	return (b.raw[internal] & (1 << offset)) != 0, nil
+}
+
+// Bytes gets bitmap
+func (b *BoolSlice) Bytes() []byte {
+	return b.raw
+}
---
+++ datastructures/boolslice/boolslice_test.go
@@ -0,0 +1,83 @@
+package boolslice
+
+import (
+	"math"
+	"testing"
+)
+
+func TestBoolSlice(t *testing.T) {
+	_, err := NewBoolSlice(12)
+	if err == nil {
+		t.Error("It should return err")
+	}
+
+	b, err := NewBoolSlice(int(math.Pow(2, 15)))
+	if err != nil {
+		t.Error("It should not return err", err)
+	}
+
+	i1 := 12
+	i2 := 20
+	i3 := 123
+	i4 := 2000
+	i5 := 8192
+
+	if err := b.Set(int(math.Pow(2, 15)) + 1); err == nil {
+		t.Error("It should return err")
+	}
+	if err := b.Set(i1); err != nil {
+		t.Error("It should not return err")
+	}
+	if err := b.Set(i2); err != nil {
+		t.Error("It should not return err")
+	}
+	if err := b.Set(i3); err != nil {
+		t.Error("It should not return err")
+	}
+	if err := b.Set(i4); err != nil {
+		t.Error("It should not return err")
+	}
+	if err := b.Set(i5); err != nil {
+		t.Error("It should not return err")
+	}
+
+	if _, err := b.Get(int(math.Pow(2, 15)) + 1); err == nil {
+		t.Error("It should return err")
+	}
+	if v, _ := b.Get(i1); !v {
+		t.Error("It should match", i1)
+	}
+	if v, _ := b.Get(i2); !v {
+		t.Error("It should match", i2)
+	}
+	if v, _ := b.Get(i3); !v {
+		t.Error("It should match", i3)
+	}
+	if v, _ := b.Get(i4); !v {
+		t.Error("It should match", i4)
+	}
+	if v, _ := b.Get(i5); !v {
+		t.Error("It should match", i5)
+	}
+	if v, _ := b.Get(200); v {
+		t.Error("It should not match 200")
+	}
+	if v, _ := b.Get(5000); v {
+		t.Error("It should not match 5000")
+	}
+
+	if len(b.Bytes()) != int(math.Pow(2, 15)/8) {
+		t.Error("Len should be 4096")
+	}
+
+	if err := b.Clear(int(math.Pow(2, 15)) + 1); err == nil {
+		t.Error("It should return err")
+	}
+	if err := b.Clear(i1); err != nil {
+		t.Error("It should not return err")
+	}
+
+	if v, _ := b.Get(i1); v {
+		t.Error("It should not match after cleared", i1)
+	}
+}
