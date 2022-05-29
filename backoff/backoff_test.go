158a9cd78a47251557a3ddef73368ef0ad2bcbac
Matias Melograno <matias.melograno@split.io>
2022-05-29 14:27:53 -0300

    Updated Backoff component to have proper base and exponential calculation. Added maximum allowed time to wait in calculation

---
+++ backoff/backoff_test.go
@@ -0,0 +1,37 @@
+package backoff
+
+import (
+	"testing"
+	"time"
+)
+
+func TestBackoff(t *testing.T) {
+	base := int64(10)
+	maxAllowed := 60 * time.Second
+	backoff := New(&base, &maxAllowed)
+	if backoff.base != base {
+		t.Error("It should be equals to 10")
+	}
+	if backoff.maxAllowed != maxAllowed {
+		t.Error("It should be equals to 60")
+	}
+	if backoff.Next() != 10*time.Second {
+		t.Error("It should be 10 seconds")
+	}
+	if backoff.Next() != 20*time.Second {
+		t.Error("It should be 20 seconds")
+	}
+	if backoff.Next() != 40*time.Second {
+		t.Error("It should be 40 seconds")
+	}
+	if backoff.Next() != 60*time.Second {
+		t.Error("It should be 60 seconds")
+	}
+	backoff.Reset()
+	if backoff.current != 0 {
+		t.Error("It should be zero")
+	}
+	if backoff.Next() != 10*time.Second {
+		t.Error("It should be 10 seconds")
+	}
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/splitio/go-toolkit/v5
 
 go 1.13
 
-require github.com/go-redis/redis/v8 v8.11.4
+require github.com/go-redis/redis/v8 v8.11.5
 
 // Version is broken
 retract v5.0.1
